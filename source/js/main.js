8f5b083f56eb28cbfa5e5eacb7e238080167eb36
Anaena <76489604+Anaena@users.noreply.github.com>
2021-09-24 12:02:57 +0000

    Deploying to gh-pages from @ Anaena/Nemova_way@a9b9117760810bf0fcd99b505f7f7395bf75513d 🚀

--- source/js/main.js
+++
@@ -1,7 +0,0 @@
-import './navigation.min.js';
-import './form.min.js';
-import './user-modal.min.js';
-import { onTabClick, onCardListClick } from './tabs.min.js';
-
-onTabClick();
-onCardListClick();
